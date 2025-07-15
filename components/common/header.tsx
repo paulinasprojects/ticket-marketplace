85f597f6625e64351ba3d44a22701348fb754d45
paulinasprojects <virtualpaula93@gmail.com>
2025-07-15 11:00:15 +0300

    implemented action buttons on desktop and mobile views

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
               {/* Renders the sign in button if you are sign out */}
             <SignedOut>
               <SignInButton mode="modal">
-                <button className="bg-gray-100 text-gray-800 px-3 py-1.5 text-sm rounded-lg hover:bg-gray-200 transition border border-gray-300">
+                <button className="bg-gray-100 text-gray-800 px-3 py-1.5 text-sm rounded-lg hover:bg-gray-200 transition border border-gray-300 cursor-pointer">
                   Sign In
                 </button>
               </SignInButton>
@@ -38,6 +38,49 @@ const Header = () => {
         {/* Search bar */}
         <div className="w-full lg:max-w-2xl">
           <Searchbar/>
+        </div>
+
+        {/* Desktop Action Buttons */}
+        <div className="hidden lg:block ml-auto">
+          <SignedIn>
+            <div className="flex items-center gap-3">
+              <Link href="/seller">
+                <button className="bg-blue-600 text-white px-3 py-1.5 text-sm rounded-lg hover:bg-blue-700 transition cursor-pointer">
+                  Sell Tickets
+                </button>
+              </Link>
+              <Link href="/tickets">
+                <button className="bg-gray-100 text-gray-800 px-3 py-1.5 text-sm rounded-lg hover:bg-gray-200 transition border border-gray-300 cursor-pointer">
+                  My Tickets
+                </button>
+              </Link>
+              <UserButton/>
+            </div>
+          </SignedIn>
+
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button className="bg-gray-100 text-gray-800 px-3 py-1.5 text-sm rounded-lg hover:bg-gray-200 transition border border-gray-300 cursor-pointer">
+                  Sign In
+                </button>
+            </SignInButton>
+          </SignedOut>
+        </div>
+
+        {/* Mobile Action Buttons */}
+        <div className="lg:hidden w-full flex justify-center gap-3">
+          <SignedIn>
+            <Link href="/seller" className="flex-1">
+              <button className="w-full cursor-pointer bg-blue-600 text-white px-3 py-1.5 text-sm rounded-lg hover:bg-blue-700 transition">
+                Sell Tickets
+              </button>
+            </Link>
+            <Link href="/tickets" className="flex-1">
+              <button className="w-full cursor-pointer bg-gray-100 text-gray-800 px-3 py-1.5 text-sm rounded-lg hover:bg-gray-200 transition border border-gray-300">
+                My Tickets
+              </button>
+            </Link>
+          </SignedIn>
         </div>
       </div>
     </div>
