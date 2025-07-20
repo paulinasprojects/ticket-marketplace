44a815058ecca31ff86b977442afffde1b30ca43
paulinasprojects <virtualpaula93@gmail.com>
2025-07-20 18:16:45 +0300

    implemented syncing users between convex & clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
 import Header from "@/components/common/header";
+import SyncUserWithConvex from "@/components/sync-user-with-convex";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <ConvexClientProvider>
         <ClerkProvider>
           <Header/>
+          <SyncUserWithConvex/>
           {children}
         </ClerkProvider>
         </ConvexClientProvider>
